8f48451efda58277690c56d6c2e1003cbe4c1ac8
Greg Answer <greganswer@gmail.com>
2018-01-01 22:03:30 -0500

    Add TestBookShow

--- handlers_test.go
+++ handlers_test.go
@@ -8,6 +8,47 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+func TestBookShow(t *testing.T) {
+	t.Log("When the books' ISDN does not exist")
+	req1, err := http.NewRequest("GET", "/books/1234", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr1 := newRequestRecorder(req1, "GET", "/books/:isdn", BookShow)
+	expCode := 404
+	if rr1.Code != expCode {
+		t.Fatalf("Expected response code to be %d, got %d", expCode, rr1.Code)
+	}
+	expRes1 := "{\"error\":{\"status\":404,\"title\":\"Record Not Found\"}}\n"
+	actual1 := rr1.Body.String()
+	if actual1 != expRes1 {
+		t.Fatalf("\nResponse body does not match: \nExpected: '%s' \n  Actual: '%s'", expRes1, actual1)
+	}
+	t.Log("When the book exists")
+	// Create an entry of the book to the bookstore map
+	testBook := &Book{
+		ISDN:   "111",
+		Title:  "test title",
+		Author: "test author",
+		Pages:  42,
+	}
+	bookstore["111"] = testBook
+	req2, err := http.NewRequest("GET", "/books/111", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr2 := newRequestRecorder(req2, "GET", "/books/:isdn", BookShow)
+	expCode2 := 200
+	if rr2.Code != expCode2 {
+		t.Fatalf("Expected response code to be %d, got %d", expCode, rr2.Code)
+	}
+	// expected response
+	expRes2 := "{\"meta\":null,\"data\":{\"isdn\":\"111\",\"title\":\"test title\",\"author\":\"test author\",\"pages\":42}}\n"
+	actual2 := rr2.Body.String()
+	if actual2 != expRes2 {
+		t.Fatalf("\nResponse body does not match: \nExpected: '%s' \n  Actual: '%s'", expRes2, actual2)
+	}
+}
 func TestBookIndex(t *testing.T) {
 	testBook := &Book{
 		ISDN:   "111",
